cede9c74422ddd2d7b9bf7933bfcd27d99d96530
Joy Huang <joy9154@gmail.com>
2025-09-22 13:05:28 -0400

    draft of new website

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
